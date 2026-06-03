60b3e01b2cfdb105534cba5281eb33746c0b1ac1
eugenyly <eugenypop@gmail.com>
2026-06-03 21:49:24 +0200

    +upd: omit;

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  const fieldsArr = Array.from(fields);
+  const result = obj;
+  for (const [key, value] of Object.entries(obj)) {
+    fieldsArr.forEach(name => {
+      if (name === key) {
+        delete result[key];
+      }
+    });
+  }
+  return result;
 };
